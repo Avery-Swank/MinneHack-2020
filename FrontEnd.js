1f612366071d562204c59855e586b308fb445542
David Wu <wuxx1751@umn.edu>
2020-01-26 00:19:05 -0600

    Working on maps and profile section

--- FrontEnd.js
+++ FrontEnd.js
@@ -6,31 +6,31 @@ function initMap() {
   			var placeTwo = {lat: 44.9731, lng: -93.2364};
   			var placeThree = {lat: 44.9731, lng: -93.2344};
   			var placeFour = {lat: 44.9721, lng: -93.2354};
-  			
+
   			// Zooming in on Coffman Memorial Union
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 44.9731, lng: -93.2354},
           zoom: 15
         });
-  			
+
   			//Call maps Marker constructor to place the map at location specified
   			var markerOne = new google.maps.Marker({
-          position: placeOne, 
+          position: placeOne,
           map: map
         });
-  
+
   			var markerTwo = new google.maps.Marker({
-          position: placeTwo, 
+          position: placeTwo,
           map: map
         });
-  
+
   			var markerThree = new google.maps.Marker({
-          position: placeThree, 
+          position: placeThree,
           map: map
         });
-  
+
   			var markerFour = new google.maps.Marker({
-          position: placeFour, 
+          position: placeFour,
           map: map
         });
       }
@@ -41,13 +41,13 @@ function addMarker(latitude, longitude) {
   // from the array of alphabetical characters.
   var marker = new google.maps.Marker({
     position: {lat: latitude, lng: longitude},
-    
+
     // LABELS = ID for marker
     // SWITCH Statements with Dogs and Cats and such, then default labels for others
     //label: labels[labelIndex++ % labels.length],
-    
-    
-    
+
+
+
     map: map
   });
 }
@@ -56,4 +56,3 @@ function addMarker(latitude, longitude) {
 function removeMarker(marker) {
   marker.setMap(null);
 }
-
