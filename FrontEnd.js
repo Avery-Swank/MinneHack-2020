8126475fe77adf460aaebdd43fc28b25a2582c27
David Wu <wuxx1751@umn.edu>
2020-01-26 13:40:08 -0600

    We Attempted to Implement Markers with Data

--- FrontEnd.js
+++ FrontEnd.js
@@ -2,54 +2,36 @@ var map;
 
 // Call this function to retrieve data on click
 function retrieveData(marker){
-  window.alert(marker.label + " position is: " + marker.getPosition());
+  window.alert(marker.getTitle() + " pet name is located at: " + marker.getPosition());
 }
 
 // Adds a marker to the map.
-function addMarker(latitude, longitude) {
+function addMarker(latitude, longitude, data) {
   // Add the marker at the clicked location, and add the next-available label
   // from the array of alphabetical characters.
   var marker = new google.maps.Marker({
     position: {lat: latitude, lng: longitude},
-
-    // LABELS = ID for marker
-    // SWITCH Statements with Dogs and Cats and such, then default labels for others
-    //label: labels[labelIndex++ % labels.length],
 		label: 'Test',
-    map: map
+    map: map,
+    Title: data.petName
+
   });
-  console.log(latitude)
-}
+};
 
 //Remove marker from map
 function removeMarker(marker) {
   marker.setMap(null);
 }
 
-
-
 //Creates the map and place markers on map for now
 function initMap() {
 		var placeOne = {lat: 44.9741, lng: -93.2354};
-		// var placeTwo = {lat: 44.9731, lng: -93.2364};
-		// var placeThree = {lat: 44.9731, lng: -93.2344};
-		// var placeFour = {lat: 44.9721, lng: -93.2354};
 
 		// Zooming in on Coffman Memorial Union
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 44.9731, lng: -93.2354},
       zoom: 15
     });
-
-		// var test = addMarker(44.9731, -93.2364);
-		// //OnClick Events that responds to a Click, displays location
-		// test.addListener('click', function() {
-		// map.setZoom(18);
-		// map.setCenter(markerOne.getPosition());
-		// //Replace code below to retrieve data and change things
-		// // setTimeout(retrieveData(markerOne), 5000)
-		// retrieveData(markerOne);
-		// });
 
   	// Jquery Post
 		$.ajax({
@@ -64,7 +46,7 @@ function initMap() {
         		for(var i = 0; i < data.length; i++) {
               const lat = parseFloat(data[i].latitude);
               const lon = parseFloat(data[i].longitude);
-              const marker = addMarker(lat, lon);
+              const marker = addMarker(lat, lon, data[i]);
             }
         }
 		})
@@ -77,24 +59,6 @@ function initMap() {
       label: 'D'
     });
 
-// 		var markerTwo = new google.maps.Marker({
-//       position: placeTwo,
-//       map: map,
-//       label: 'D2'
-//     });
-
-// 		var markerThree = new google.maps.Marker({
-//       position: placeThree,
-//       map: map,
-//       label: 'D3'
-//     });
-
-// 		var markerFour = new google.maps.Marker({
-//       position: placeFour,
-//       map: map,
-//       label: 'D4'
-//     });
-
     //OnClick Events that responds to a Click, displays location
     markerOne.addListener('click', function() {
       map.setZoom(18);
@@ -102,20 +66,8 @@ function initMap() {
       //Replace code below to retrieve data and change things
       // setTimeout(retrieveData(markerOne), 5000)
       retrieveData(markerOne);
+
+
+
       });
-    // markerTwo.addListener('click', function() {
-    //   map.setZoom(18);
-    //   map.setCenter(markerTwo.getPosition());
-    //   retrieveData(markerTwo);
-    //   });
-    // markerThree.addListener('click', function() {
-    //   map.setZoom(18);
-    //   map.setCenter(markerThree.getPosition());
-    //   retrieveData(markerThree);
-    //   });
-    // markerFour.addListener('click', function() {
-    //   map.setZoom(18);
-    //   map.setCenter(markerFour.getPosition());
-    //   retrieveData(markerFour);
-    //   });
   }
