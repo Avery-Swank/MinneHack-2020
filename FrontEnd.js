a3348d47daf811431a6b9f92df12477bfaeb985a
Thomas Nguyen <tpnguyen1999@gmail.com>
2020-01-25 22:01:25 -0600

    FrontEnd #3

---
+++ FrontEnd.js
@@ -0,0 +1,9 @@
+var map;
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 44.9731, lng: -93.2354},
+          zoom: 15
+        });
+      }
+
